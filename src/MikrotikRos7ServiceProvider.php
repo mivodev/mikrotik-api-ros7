513a0e9180b9b96732e2b01e67c72920635a6aee
dyzulk <66510723+dyzulk@users.noreply.github.com>
2026-05-27 21:29:59 +0700

    chore: initialize package structure

---
+++ src/MikrotikRos7ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mivo\MikrotikRos7;
+
+use Illuminate\Support\ServiceProvider;
+
+class MikrotikRos7ServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Register ros7 services
+    }
+
+    public function boot(): void
+    {
+        // Boot ros7 services
+    }
+}
